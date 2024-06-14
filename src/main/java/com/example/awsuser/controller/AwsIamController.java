97bf05246baa06bb45bff732ae071903bbe25bea
YoungdonJung <yeavov@gmail.com>
2024-06-14 21:34:57 +0900

    동작 별 주석 추가

--- src/main/java/com/example/awsuser/controller/AwsIamController.java
+++ src/main/java/com/example/awsuser/controller/AwsIamController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.services.iam.model.AccessKeyMetadata;
 
 import java.util.List;
 
@@ -16,6 +15,7 @@ public class AwsIamController {
 
     private final AwsIamService awsIamService;
 
+    //AWS IAM에서 생성한지 N시간을 초과하는 IAM User의 User ID와 Access Key ID를 반환
     @GetMapping("/old-create-keys")
     public List<UserAccessKeyInfo> getOldCreateKeys(@RequestParam int hours) {
         return awsIamService.getOldCreateKeys(hours);
--- src/main/java/com/example/awsuser/service/AwsIamService.java
+++ src/main/java/com/example/awsuser/service/AwsIamService.java
@@ -23,17 +23,19 @@ public class AwsIamService {
         ListUsersResponse listUsersResponse = iamClient.listUsers();
         Instant cutoffTime = Instant.now().minus(hours, ChronoUnit.HOURS);
 
+        // 정보를 받아온 IAM 유저들의 UserID와 Access Key를 확인합니다.
         for(User user : listUsersResponse.users()){
             ListAccessKeysResponse listAccessKeysResponse = iamClient.listAccessKeys(
                     ListAccessKeysRequest.builder().userName(user.userName()).build());
 
+            //각 AccessKey의 생성 날짜를 확인하고, 요청된 시간보다 오래된 경우 결과에 추가합니다.
             for (AccessKeyMetadata accessKey : listAccessKeysResponse.accessKeyMetadata()){
                 if (accessKey.createDate().isBefore(cutoffTime)) {
                     oldCreateKeys.add(new UserAccessKeyInfo(user.userName(), accessKey.accessKeyId(), accessKey.createDate()));
                 }
             }
         }
-
+        //createDate 기준으로 정렬합니다.
         Collections.sort(oldCreateKeys);
 
         return oldCreateKeys;
