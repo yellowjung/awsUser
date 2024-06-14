554b1dcdf188f0c1a93842a14e9a8af79376a8b4
youngdonJung <youngdon.jung@lotte.net>
2024-06-14 14:47:13 +0900

    Create Project
    생성된지 특정 일자 이후인 IAM User의 정보를 가져오는 프로젝트

---
+++ src/main/java/com/example/awsuser/AwsUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsuser/AwsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
